6a630c122ede9b046fc44a31808ab194e83bd0ff
Bogdan Marković <95984932+Gibo2706@users.noreply.github.com>
2024-04-08 16:11:19 +0200

    reformating

--- Shared/BeleskeContext.cs
+++ Shared/BeleskeContext.cs
@@ -31,7 +31,7 @@ public partial class BeleskeContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=seminarskic.database.windows.net; Database=Beleske; User=Jasmin;Password=Beleske123!jevrej; Trusted_Connection=false; Encrypt=true; TrustServerCertificate=true", x => x.UseDateOnlyTimeOnly());
+        => optionsBuilder.UseSqlServer("Server=seminarskic.database.windows.net; Database=Beleske; User=User;Password=Password; Trusted_Connection=false; Encrypt=true; TrustServerCertificate=true", x => x.UseDateOnlyTimeOnly());
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
