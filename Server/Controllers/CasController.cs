3582165e307bf330d0d26534392cfa1e65522ca4
Nikola <kuzmicnikolasrb@gmail.com>
2024-04-10 21:53:42 +0200

    Dodati osnovni cas i predmet kontroleri. Izmenjen DataContext da podrzava i predmet.

---
+++ Server/Controllers/CasController.cs
@@ -0,0 +1,24 @@
+﻿using BeleskeBlazor.Server.Data;
+using BeleskeBlazor.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeleskeBlazor.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CasController : Controller
+    {
+        private readonly DataContext _context;
+       
+        public CasController(DataContext context) { 
+            _context = context;
+        }
+
+        public async Task<ActionResult<List<Cas>>> GetAllCas()
+        {
+            var list = await _context.Cas.ToListAsync();
+            return Ok(list);
+        }
+    }
+}
---
+++ Server/Controllers/PredmetController.cs
@@ -0,0 +1,34 @@
+﻿using BeleskeBlazor.Server.Data;
+using BeleskeBlazor.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
+namespace BeleskeBlazor.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PredmetController : Controller
+    {
+        private readonly DataContext _context;
+
+        public PredmetController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ActionResult<List<Cas>>> GetCasoviPredmeta(int id)
+        {
+            var list = _context.Predmet.Find(id)
+                        .DrziUsemestrus
+                        .SelectMany(dus=>dus.Cas);
+            return Ok(list);
+        }
+
+        public async Task<ActionResult<List<Predmet>>> GetAllPredmeti()
+        {
+            var list = await _context.Predmet.ToListAsync();
+            return Ok(list);
+        }
+    }
+}
