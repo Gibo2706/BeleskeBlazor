04ef8fa78dc4fdc3204fecb7c4a196d64c3c66bd
Nikola <kuzmicnikolasrb@gmail.com>
2024-04-11 20:20:18 +0200

    Controller-repository pattern  pocetak

---
+++ Server/Controllers/SemestarController.cs
@@ -0,0 +1,29 @@
+﻿using BeleskeBlazor.Server.Repositoriums;
+using BeleskeBlazor.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeleskeBlazor.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SemestarController : Controller
+    {
+        private readonly SemestarRepo _semRepo;
+        
+        public SemestarController(SemestarRepo semRepo)
+        {
+            _semRepo = semRepo;
+        }
+
+
+        [Route("getSemestriPredmeta")]
+        public async Task<ActionResult<List<Semestar>>> getSemestriPredmeta(int id)
+        {
+            var list = await _semRepo.GetSemestriPredmeta(id);
+
+            return Ok(list);
+        }
+
+    }
+}
---
+++ Server/Repositoriums/SemestarRepo.cs
@@ -0,0 +1,25 @@
+﻿using BeleskeBlazor.Server.Data;
+using BeleskeBlazor.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BeleskeBlazor.Server.Repositoriums
+{
+    public class SemestarRepo
+    {
+        private readonly DataContext _context;
+
+        public SemestarRepo(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Semestar>> GetSemestriPredmeta(int id)
+        {
+            List <Semestar> list = _context.Predmet.Find(id)
+                        .DrziUsemestrus
+                        .Select(dus => dus.IdSemestarNavigation).ToList<Semestar>();
+            return list;
+        }
+    }
+}
